f955602f8d41be17807cf77d81cd2a8a7f8c3a24
Shaili patel <48171767+cherry247@users.noreply.github.com>
2021-05-28 12:21:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,62 @@
-print('hi')
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS
+import numpy as np
+
+#title
+st.title('Tweet Sentiment Analysis')
+#markdown
+st.markdown('This application is all about tweet sentiment analysis of airlines. We can analyse reviews of the passengers using this streamlit app.')
+#sidebar
+st.sidebar.title('Sentiment analysis of airlines')
+# sidebar markdown 
+st.sidebar.markdown("🛫We can analyse passengers review from this application.🛫")
+#loading the data (the csv file is in the same folder)
+#if the file is stored the copy the path and paste in read_csv method.
+data=pd.read_csv('tweet.csv')
+#checkbox to show data 
+if st.checkbox("Show Data"):
+    st.write(data.head(50))
+#subheader
+st.sidebar.subheader('Tweets Analyser')
+#radio buttons
+tweets=st.sidebar.radio('Sentiment Type',('positive','negative','neutral'))
+st.write(data.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
+st.write(data.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
+st.write(data.query('airline_sentiment==@tweets')[['text']].sample(1).iat[0,0])
+#selectbox + visualisation
+# An optional string to use as the unique key for the widget. If this is omitted, a key will be generated for the widget based on its content.
+## Multiple widgets of the same type may not share the same key.
+select=st.sidebar.selectbox('Visualisation Of Tweets',['Histogram','Pie Chart'],key=1)
+sentiment=data['airline_sentiment'].value_counts()
+sentiment=pd.DataFrame({'Sentiment':sentiment.index,'Tweets':sentiment.values})
+st.markdown("###  Sentiment count")
+if select == "Histogram":
+        fig = px.bar(sentiment, x='Sentiment', y='Tweets', color = 'Tweets', height= 500)
+        st.plotly_chart(fig)
+else:
+        fig = px.pie(sentiment, values='Tweets', names='Sentiment')
+        st.plotly_chart(fig)
+
+#slider
+st.sidebar.markdown('Time & Location of tweets')
+hr = st.sidebar.slider("Hour of the day", 0, 23)
+data['Date'] = pd.to_datetime(data['tweet_created'])
+hr_data = data[data['Date'].dt.hour == hr]
+if not st.sidebar.checkbox("Hide", True, key='1'):
+    st.markdown("### Location of the tweets based on the hour of the day")
+    st.markdown("%i tweets during  %i:00 and %i:00" % (len(hr_data), hr, (hr+1)%24))
+    st.map(hr_data)
+
+#multiselect
+st.sidebar.subheader("Airline tweets by sentiment")
+choice = st.sidebar.multiselect("Airlines", ('US Airways', 'United', 'American', 'Southwest', 'Delta', 'Virgin America'), key = '0')  
+if len(choice)>0:
+    air_data=data[data.airline.isin(choice)]
+    # facet_col = 'airline_sentiment'
+    fig1 = px.histogram(air_data, x='airline', y='airline_sentiment', histfunc='count', color='airline_sentiment',labels={'airline_sentiment':'tweets'}, height=600, width=800)
+    st.plotly_chart(fig1)
+
+
